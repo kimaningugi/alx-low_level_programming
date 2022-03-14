e98dfe820a881c3b045b9a5cba7a327e9ad9472b
root <victorngugikim@gmail.com>
2022-03-14 10:18:13 -0700

    C programming scripts

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints sizes of different data types
+* Return: 0
+*/
 int main(void)
 {
 	char c;
