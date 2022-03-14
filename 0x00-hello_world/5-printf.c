6e65d7837cd688471455e2c7c4d88d1fd95371fa
root <victorngugikim@gmail.com>
2022-03-14 10:15:14 -0700

    C programming scripts

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
-
+/**
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "with proper grammar, but the outcome is a piece of art,\n"
+* Return: 0
+*/
 int main(void)
+
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
