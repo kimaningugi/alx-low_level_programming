d50498c6a912d2c469d638f4ea04dd597a54db95
root <victorngugikim@gmail.com>
2022-03-20 02:32:02 -0700

    C - functions and prototypes

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * function to print abc
+ * return: 0
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+_putchar('\n');
+}	
