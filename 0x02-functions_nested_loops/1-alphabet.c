0964e8127179fdb5bc9253a30e8c838ebf181024
kimaningugi <victorngugikim@gmail.com>
2022-06-12 06:55:51 -0700

    Functions and Nested Loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,13 +2,12 @@
 /**
  * print_alphabet function - prints alphabet in lowercase
  * followed by a new line
- * 
+ *
  * Description - prints alphabet in lowercase
  */
 void print_alphabet(void)
 {
 	char x;
-
 	x = 'a';
 
 	while (x <= 'z')
